ff6fff297b1e8e3d644832704e0294c2215ff492
Dinna Esayas <dinnaesayas@hotmail.com>
2016-03-02 14:45:18 -0500

    I created a hello world file.

---
+++ ICS3U/src/esayas/HelloWorld.java
@@ -0,0 +1,14 @@
+package esayas;
+/*HelloWorld.java
+ * this program prints hello world to the console
+ * Dinna Esayas
+ * March 2nd, 2016*/
+
+  public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+             System.out.println("Hello World");
+	}
+
+}
